553b739c94370a226fa32e97c4a87d6601f2b7e8
yoon <yunkh2005@naver.com>
2025-10-14 23:38:28 +0900

    next 3.4 done

--- next/app/(movies)/movies/[id]/page.tsx
+++ next/app/(movies)/movies/[id]/page.tsx
@@ -1,11 +1,33 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fwetcing movies: ${Date.now()}`);
+  await new Promise((resolve => setTimeout(resolve, 3000)));
+  const response = await fetch(`${API_URL}/${id}`);
+
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fwetcing videos: ${Date.now()}`);
+  await new Promise((resolve => setTimeout(resolve, 3000)));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: {
     id,
   },
 }: {
   params: { id: string };
 }) {
+  console.log('start');
+  const [movie, video] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log('end');
+
   return (
-    <h1>Movie {id}</h1>
+    <h1>{movie.title}</h1>
   )
 }
