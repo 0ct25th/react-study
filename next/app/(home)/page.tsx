6bf423011a85aa7e6a2996be6c8d62a0d53cc989
yoon <yunkh2005@naver.com>
2025-10-14 22:32:47 +0900

    next 3.2 done

--- next/app/(home)/page.tsx
+++ next/app/(home)/page.tsx
@@ -1,24 +1,22 @@
-"use client"
+export const metadata = {
+  title: "Home",
+}
 
-import { useEffect, useState } from "react";
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies'
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState();
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
 
-  const getMovies = async () => {
-    const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies')
-    const json = await response.json()
-    setMovies(json);
-    setIsLoading(false);
-  }
+  return json
+}
 
-  useEffect(() => {
-    getMovies();
-  })
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      {isLoading ? "Loading..." : JSON.stringify(movies)}
+      {JSON.stringify(movies)}
     </div>
   )
 }
