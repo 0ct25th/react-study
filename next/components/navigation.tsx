0edc54b92c502df459b11e589fa6b7eef585b2f0
yoon <yunkh2005@naver.com>
2025-10-14 16:56:16 +0900

    next 2.2 done

---
+++ next/components/navigation.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/">Home {path === "/" ? "🏠" : ""}</Link></li>
+        <li><Link href="/about-us">About us {path === "/about-us" ? "🫂" : ""}</Link></li>
+      </ul>
+    </nav>
+  )
+}
